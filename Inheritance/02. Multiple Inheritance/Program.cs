430a158b3aba966f60c35773e045837ae9c7f70d
Denitsa Velikova <deniitooou966@mail.bg>
2022-11-01 20:56:16 +0200

    Inheritance - Lab
    Inheritance - Lab

---
+++ Inheritance/02. Multiple Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Weep();
+
+        }
+    }
+}
