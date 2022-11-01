2154e4ff3913948025009ed52c16b77380d6e6bb
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-01 22:18:07 +0200

    Inheritance - Exercise
    Inheritance - Exercise

---
+++ Inheritance - Exercise/NeedForSpeed/Motorcycles/RaceMotorcycle.cs
@@ -0,0 +1,10 @@
+﻿namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double DEFAULT_RACE_MOTORCYCLE_FUEL_CONSUMPTION = 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel){}
+
+        public override double FuelConsumption => DEFAULT_RACE_MOTORCYCLE_FUEL_CONSUMPTION;
+    }
+}
