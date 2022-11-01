af1be8da227b9cda3d28ce87730e056845dde078
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-01 22:17:50 +0200

    Delete Inheritance - Exercise/NeedForSpeed directory

--- Inheritance - Exercise/NeedForSpeed/Car.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NeedForSpeed
-{
-    public class Car : Vehicle
-    {
-        private const double DEFAULT_CAR_FUEL_CONSUMPTION = 3;
-        public Car(int horsePower, double fuel) : base(horsePower, fuel) {}
-        public override double FuelConsumption => DEFAULT_CAR_FUEL_CONSUMPTION;
-    }
-}
