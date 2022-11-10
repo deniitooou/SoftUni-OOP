077b2cce35bc705ccd4748812b4d5d0cb3112303
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-10 16:34:30 +0200

    Interfaces and Abstraction - Exercise
    Interfaces and Abstraction - Exercise

---
+++ Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/Models/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name => this.name;
+
+        public int Age => this.age;
+    }
+}
---
+++ Interfaces and Abstraction - Exercise/02. Multiple Implementation/Models/Citizen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+            this.birthdate = birthdate;
+        }
+
+        public string Name => this.name;
+        public int Age => this.age;
+        public string Id => this.id;
+        public string Birthdate => this.birthdate;
+    }
+}
