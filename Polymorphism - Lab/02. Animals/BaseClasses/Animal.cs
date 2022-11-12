d46a651bf717346ca43d7211b7d602f644bfb57b
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-12 02:01:22 +0200

    Polymorphism - Lab
    Polymorphism - Lab

---
+++ Polymorphism - Lab/02. Animals/BaseClasses/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string Name;
+        private string FavouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf ()
+        {
+            return ($"I am {this.Name} and my favourite food is {this.FavouriteFood}");
+            
+        }
+    }
+
+}
+
---
+++ Polymorphism - Lab/03. Shapes/Models/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle (double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea() => Math.PI * Math.Pow(radius, 2);
+
+        public override double CalculatePerimeter() => 2 * Math.PI * radius;
+        
+        public override string Draw() => "Drawing " + this.GetType().Name;
+    }
+}
