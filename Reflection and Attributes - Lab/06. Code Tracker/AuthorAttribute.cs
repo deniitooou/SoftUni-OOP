db43757cdbd84821b9cb9d8d441928d41ba2866f
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-22 17:54:50 +0200

    Reflection and Attributes - Lab
    Reflection and Attributes - Lab

---
+++ Reflection and Attributes - Lab/06. Code Tracker/AuthorAttribute.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AuthorProblem
+{
+    using System;
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public AuthorAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthorProblem
+{
+    [Author("John")]
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+
+        [Author("Jane")]
+        public void Test() { }
+    }
+}
