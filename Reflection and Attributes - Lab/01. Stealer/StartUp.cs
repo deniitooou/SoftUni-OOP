980fe2db73cd2244d03b9ab632aa94d321f1030c
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-22 17:40:33 +0200

    Reflection and Attributes - Lab
    Reflection and Attributes - Lab

---
+++ Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/03. Mission Private Impossible/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace MissionPrivateImpossible
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
