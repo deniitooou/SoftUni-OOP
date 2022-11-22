600e72334a9534cf972a429ec7c04e65a5076e04
Denitsa Velikova <105812554+deniitooou@users.noreply.github.com>
2022-11-22 17:44:53 +0200

    Reflection and Attributes - Lab
    Reflection and Attributes - Lab

---
+++ Reflection and Attributes - Lab/04. Collector/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Collector
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
